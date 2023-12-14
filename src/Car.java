666a8836e35db7e6faef0ad504f4e3ff4e757010
ruslanshanuk@gmail.com <ruslanshanuk@gmail.com>
2023-12-14 12:14:03 +0100

    creating class Vehicle

---
+++ src/Car.java
@@ -0,0 +1,16 @@
+//public class Car extends Vehicle{
+//    public String model;
+//
+//    public Car(String brand, int year, String model) {
+//        super(brand, year);
+//        this.model = model;
+//    }
+//    @Override
+//    public String toString() {
+//        return "Марка : " + brand + " Модель : " + model + " год : " + year;
+//    }
+//    @Override
+//    public void displayInfo() {
+//        System.out.println("Марка : " + brand + "год : " + year );
+//    }
+//}
---
+++ src/Vehicle.java
@@ -0,0 +1,12 @@
+public class Vehicle {
+    public String brand;
+    public int year;
+
+//    public Vehicle(String brand, int year) {
+//        this.brand = brand;
+//        this.year = year;
+//    }
+// public void displayInfo() {
+//     System.out.println("Марка : " + brand + "год : " + year );
+//    }
+}
