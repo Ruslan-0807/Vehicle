14c5ab55c55a416fcb6a93ebd4c2a5fd17e21be2
ruslanshanuk@gmail.com <ruslanshanuk@gmail.com>
2023-12-14 12:53:30 +0100

    add method displayInfo

--- src/Vehicle.java
+++ src/Vehicle.java
@@ -2,11 +2,11 @@ public class Vehicle {
     public String brand;
     public int year;
 
-//    public Vehicle(String brand, int year) {
-//        this.brand = brand;
-//        this.year = year;
-//    }
-// public void displayInfo() {
-//     System.out.println("Марка : " + brand + "год : " + year );
-//    }
+    public Vehicle(String brand, int year) {
+        this.brand = brand;
+        this.year = year;
+    }
+ public void displayInfo() {
+     System.out.println("Марка : " + brand + "год : " + year );
+    }
 }
